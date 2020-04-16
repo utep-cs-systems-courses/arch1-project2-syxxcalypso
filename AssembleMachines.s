67f1482fa5170e6dcd2e66a460bd26c62bcedf99
student <jaharrison@miners.utep.edu>
2020-04-15 23:52:00 -0600

    .

--- AssembleMachines.s
+++ AssembleMachines.s
@@ -1,8 +1,11 @@
 	.arch msp430g2553
 	.p2align 1,0
 
+	.data
+s:
+	.word 0
 
-	.text
+.text
 	.global update	;; visible to other object files
 jt:
 	.word default
@@ -10,30 +13,37 @@ jt:
 	.word case2
 
 update:	
-	mov.b &0x0021, r13		;; move s into r12
-	cmp #3, r1		;; if state > 3, !borrow 
+				;;mov.b &0x0021, r13	
+	mov.b s, r13
+	;;cmp #3, r13		 
 	jnc default
 
-	add r13, r13		;;index r12 entries. 2 bytes get the offset bytes in table
-	mov jt(r13),r0		;;r12: address of the entry, move to program counter
+	add r13, r13		
+	mov jt(r13),r0		
 
 case1:
 	mov.b #0, r12		;;led-red
-	call #set_red	
-	mov.b #1, r12
 	call #set_green	
-	jmp end		
-
-case2:			
-	mov.b #0, r12
-	call #set_green
 	mov.b #1, r12
 	call #set_red
+	mov.b #2, s
+	jmp end		
+
+case2:		
+	mov.b #1, r12
+	call #set_green
+	mov.b #0, r12
+	call #set_red
+	mov.b #1, s
 	jmp end	
 
 default:
+
 	mov.b #1, r12
 	call #set_green
+	mov.b #1, r13
+	mov.b r13, s
 	jmp end
+
 end:
 	pop r0	
